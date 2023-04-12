653da2d102496746efc3e0ba890059fd82212e9e
Omelianov Nicolai <omelianov.n@gmail.com>
2023-04-13 00:07:20 +0300

    fixed api image-base-url

--- src/js/api-vars.js
+++ src/js/api-vars.js
@@ -3,6 +3,6 @@ const BASE_URL = 'https://api.themoviedb.org/3';
 const TREND_URL = `${BASE_URL}/trending/movie/week`;
 const SEARCH_URL = `${BASE_URL}/search/movie`;
 const ID_URL = `${BASE_URL}/movie/`;
-const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500/'
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500'
 
 export { API_KEY, BASE_URL, TREND_URL, SEARCH_URL, ID_URL, IMAGE_BASE_URL };

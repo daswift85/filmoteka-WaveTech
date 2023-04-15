734f96ca885938a440c27add25bd7c6c0f76c465
daswift85 <114247464+daswift85@users.noreply.github.com>
2023-04-15 16:51:37 +0300

    Update refs.js

--- src/js/refs.js
+++ src/js/refs.js
@@ -2,6 +2,7 @@ const refs = {
   gallery: document.querySelector('.js-gallery'),
   loader: document.querySelector('.loader'),
   spinner: document.querySelector('[data-index="spinner"]'),
+  pagination: document.querySelector('#pagination'),
 };
 
 export default refs;

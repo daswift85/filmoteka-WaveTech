aaba0a903a6bde7125686d4864cb1c0ed0789b76
Nikita Tsurikhin <tsurikhinnikita@gmail.com>
2023-04-16 02:13:15 +0600

    adaptive-team-modal

--- src/js/team-modal.js
+++ src/js/team-modal.js
@@ -1,11 +1,18 @@
 const teamModalCloseBtnEl = document.querySelector('.modal-close-btn');
 const teamModalOpenLinkEl = document.querySelector('.team-modal-link');
 const teamBackdropEl = document.querySelector('.team-backdrop');
+const memberLinkEl = document.querySelectorAll('.member-link');
 
 teamModalCloseBtnEl.addEventListener('click', toggleModal);
 teamModalOpenLinkEl.addEventListener('click', toggleModal);
+memberLinkEl.forEach(element => {
+  element.addEventListener('click', function (event) {
+    event.preventDefault();
+  });
+});
 
 function toggleModal(event) {
   event.preventDefault();
   teamBackdropEl.classList.toggle('visually-hidden');
+  document.body.classList.toggle('modal-open');
 }

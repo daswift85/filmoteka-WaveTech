1b451467b23b1fc8eb0008c8e6596d72e489c32e
Sultan <a_sultan_93@mail.ru>
2023-04-16 17:34:29 +0500

    modal-new file js

--- src/js/refs.js
+++ src/js/refs.js
@@ -6,6 +6,12 @@ const refs = {
   searchForm: document.querySelector('.search__form'),
   searchInput: document.querySelector('.search__input'),
   searchSubmitButton: document.querySelector('.search__submit-button'),
+  loaderModal: document.querySelector('.modal__loader'),
+  libraty: document.querySelector('#gallery-lib'),
+  closeModalBtn: document.querySelector('.modal__btn-close'),
+  divBackdrop: document.querySelector('.js-backdrop'),
+  modalRef: document.querySelector('.modal__wrap'),
+  teamRef: document.querySelector('.team__wrap')
 };
 
 export default refs;

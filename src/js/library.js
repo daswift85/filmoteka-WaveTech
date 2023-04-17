1b30bbddd501249c87ca1a89313829bdec814b88
daswift85 <daswift85@gmail.com>
2023-04-17 18:21:17 +0300

    Update library.js

--- src/js/library.js
+++ src/js/library.js
@@ -1,16 +1,38 @@
+// import { getMovieInfo } from "./api/movieAPI";
+// import refs from "./refs";
+// import { createGalleryMarkup } from "./render/markup-cards-main-page";
+
+
+// export function takeFromLocal(key) {
+//     const existingMv = JSON.parse(localStorage.getItem(key));
+//     console.log(existingMv);
+//     let movies = [];
+//     for (const movie of existingMv) {
+//         getMovieInfo(movie).then(data => {
+//             return movies.push(data)
+//         })
+//     }
+//     return movies;
+// };
+
+// const watchedArray = takeFromLocal('watchedId');
+
+// console.log(watchedArray);
+
 import { getMovieInfo } from "./api/movieAPI";
 import refs from "./refs";
 import { createGalleryMarkup } from "./render/markup-cards-main-page";
-
 
 export function takeFromLocal(key) {
     const existingMv = JSON.parse(localStorage.getItem(key));
     console.log(existingMv);
     let movies = [];
-    for (const movie of existingMv) {
-        getMovieInfo(movie).then(data => {
-            return movies.push(data)
-        })
+    if (existingMv) { // Check if existingMv is not null or undefined
+        for (const movie of existingMv) {
+            getMovieInfo(movie).then(data => {
+                movies.push(data);
+            });
+        }
     }
     return movies;
 };
